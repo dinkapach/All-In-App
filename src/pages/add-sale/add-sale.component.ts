6bb3ba1bc931fced035f75ec8b631decec1ff692
gallib2 <gallib2@gmail.com>
2017-09-06 08:26:07 +0300

    fix add sale

--- src/pages/add-sale/add-sale.component.ts
+++ src/pages/add-sale/add-sale.component.ts
@@ -3,11 +3,11 @@ import { ManagerService } from './../../services/manager.service';
 import { UserService } from './../../services/user.service';
 import { User } from './../../models/user.model';
 import { Http } from '@angular/http';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'; 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NavController, NavParams, AlertController, ActionSheetController } from 'ionic-angular';
-import {Observable} from 'rxjs/Observable';
-import {CreditsCardComponent} from  '../credits-card/credits.card.component';
-import {UserCardComponent} from  '../user-card/usercard.component';
+import { Observable } from 'rxjs/Observable';
+import { CreditsCardComponent } from '../credits-card/credits.card.component';
+import { UserCardComponent } from '../user-card/usercard.component';
 
 import 'rxjs/add/operator/map';
 import { ClubCardComponent } from '../clubs/club-card/club.card.component';
@@ -22,51 +22,54 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'add-sale',
-    templateUrl : 'add-sale.html'
+    templateUrl: 'add-sale.html'
 })
-export class AddSaleComponent{
-    formData : FormGroup;
+export class AddSaleComponent {
+    formData: FormGroup;
     sale = new Sale();
-  
-     constructor(private fBuilder : FormBuilder, private http: Http, 
-     private managerService: ManagerService, private navCtrl : NavController,
-      private alertCtrl: AlertController, private cameraService: CameraService,
-       public actionSheetCtrl: ActionSheetController) {
-        this.formData = fBuilder.group ({
-            'id' :Date.now(),
-            'name': ["", Validators.required],
-            'description' : ["", Validators.required],
-            'price' : ["", Validators.required],
-            'points' : ["", Validators.required],
-            // 'img': [""],
-            
-        })
-     }
 
-     onClickAddSale(){
-         this.sale.id = Date.now();
+    constructor(private fBuilder: FormBuilder, private http: Http,
+        private managerService: ManagerService, private navCtrl: NavController,
+        private alertCtrl: AlertController, private cameraService: CameraService,
+        public actionSheetCtrl: ActionSheetController) {
+        this.formData = fBuilder.group({
+            'id': Date.now(),
+            'name': ["", Validators.required],
+            'description': ["", Validators.required],
+            'price': ["", Validators.required],
+            'points': ["", Validators.required],
+            // 'img': [""],
+
+        })
+    }
+
+    onClickAddSale() {
+        this.sale.id = Date.now();
         // this.sale.id =  this.formData.value.id;
         //this.sale.img //.= this.formData.value.img;
         // this.sale.name = this.formData.value.name;
         // this.sale.points = this.formData.value.points;
         // this.sale.description = this.formData.value.description;
         // this.sale.price = this.formData.value.price;
-        console.log("Added sale = ",this.sale);
+        console.log("Added sale = ", this.sale);
         this.managerService.addSale(this.sale)
-        .subscribe(sale =>{
-            console.log("added sale: ", sale);
-            this.navCtrl.pop();
-        });  
+            .subscribe(sale => {
+                console.log("sale issss:", sale);
+                if (sale) {
+                    console.log("added sale: ", sale);
+                    this.navCtrl.pop();
+                }
+            });
         // this.presentAlert();
 
     }
 
-            // to do
+    // to do
     updateImg(url) {
         console.log("in save img")
         this.sale.img = url;
     }
-        
+
     onClickOpenCameraOptionTake() {
         let actionSheet = this.actionSheetCtrl.create({
             title: 'Choose Camera Option',
